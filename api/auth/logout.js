4eb6090ea988c60482b718cea1885bea740230c8
KYYscpe <goodelem41@gmail.com>
2026-01-08 21:46:16 +0700

    Delete api directory

--- api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-import { destroySession } from "../../lib/auth.js";
-
-export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-  await destroySession(req, res);
-  res.status(200).json({ ok: true });
-}
--- api/auth/me.js
+++
@@ -1,6 +0,0 @@
-import { getSessionUser } from "../../lib/auth.js";
-
-export default async function handler(req, res) {
-  const user = await getSessionUser(req);
-  res.status(200).json({ user });
-}
